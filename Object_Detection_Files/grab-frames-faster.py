5f04106bfcd23481bbd925779c5035963453906e
grussdorian <hardikghoshal@gmail.com>
2023-05-09 21:15:07 +0530

    fixed bug with area and direction on windows, this code does not work on macos

---
+++ Object_Detection_Files/grab-frames-faster.py
@@ -0,0 +1,137 @@
+import cv2
+import threading
+import queue
+from object_ident import getObjects
+import cv2 
+frame_height = 1280
+frame_width = 720
+thresh = 0.7  # Threshold to detect object
+nms = 0.8
+fps = 1000
+delay_time = int(1000/fps)
+frame_center = (frame_height//2, frame_width//2)
+
+# Define camera capture thread
+class CaptureThread(threading.Thread):
+    def __init__(self, cap, frame_buffer):
+        threading.Thread.__init__(self)
+        self.cap = cap
+        self.frame_buffer = frame_buffer
+        self._stop_event = threading.Event()
+    def run(self):
+        while not self._stop_event.is_set():
+            # Read frame from camera
+            ret, frame = self.cap.read()
+
+            # Add frame to buffer
+            if ret:
+                self.frame_buffer.put(frame)
+
+    def stop(self):
+        self._stop_event.set()
+
+# Define frame processing thread
+class ProcessThread(threading.Thread):
+    def __init__(self, frame_buffer, processed_buffer):
+        threading.Thread.__init__(self)
+        self.frame_buffer = frame_buffer
+        self.processed_buffer = processed_buffer
+        self._stop_event = threading.Event()
+
+    def run(self):
+        while not self._stop_event.is_set():
+            # Get frame from buffer
+            frame = self.frame_buffer.get()
+
+            # Process frame
+            processed_frame = process_frame(frame)
+
+            # Add processed frame to buffer
+            self.processed_buffer.put(processed_frame)
+
+    def stop(self):
+        self._stop_event.set()
+
+# Define frame display thread
+class DisplayThread(threading.Thread):
+    def __init__(self, processed_buffer):
+        threading.Thread.__init__(self)
+        self.processed_buffer = processed_buffer
+        self._stop_event = threading.Event()
+
+    def run(self):
+        while not self._stop_event.is_set():
+            # Get processed frame from buffer
+            processed_frame = self.processed_buffer.get()
+
+            # Display processed frame
+            cv2.imshow("Processed Frame", processed_frame)
+
+            # Wait for key press
+            key = cv2.waitKey(delay_time)
+
+            # Check for 'q' key press
+            if key == ord('q'):
+                break
+
+    def stop(self):
+        self._stop_event.set()
+
+
+
+# Define frame processing function
+def process_frame(frame):
+    img = frame
+    n_objects = 1
+    result, objectInfo = getObjects(n_objects, img, thresh, nms,  frame_height=frame_height, frame_width=frame_width, draw=False)
+    # print(objectInfo)
+    ## Check iff one objected is detected. If number of objects detected != 1 then continue in the loop
+    if len(objectInfo) != 3:
+      return img
+    object_center = objectInfo[-1]
+    object_area = objectInfo[-2]/1000
+    # print(object_center)
+    # print(type(object_center[0]), type(object_center[1]))
+
+    ## Vector v denotes the magnitude and direction of Yaw control
+    v = (object_center, (frame_center[0], object_center[1]))
+    v_mod = frame_center[0] - object_center[0]
+
+    # Only try to draw stuff that are well defined otherwise opencv will crash
+    if v_mod < 0:
+        print(f"direction is left to right, magnitude is {v_mod}, area of object = {object_area}")
+    if v_mod > 0:
+        print(f"direction is right to left, magnitude is {v_mod}, area of object = {object_area}")
+    
+    cv2.arrowedLine(img, object_center, (frame_center[0], object_center[1]), (255, 0, 0),
+                                thickness=2, tipLength=0.5)
+
+
+    return img
+
+# Create camera capture object
+cap = cv2.VideoCapture(0)
+cap.set(3, frame_height)
+cap.set(4, frame_width)
+# Create frame buffers
+frame_buffer = queue.Queue(maxsize=1)
+processed_buffer = queue.Queue(maxsize=1)
+
+# Create threads
+capture_thread = CaptureThread(cap, frame_buffer)
+process_thread = ProcessThread(frame_buffer, processed_buffer)
+display_thread = DisplayThread(processed_buffer)
+
+# Start threads
+capture_thread.start()
+process_thread.start()
+display_thread.start()
+
+# Wait for threads to finish
+capture_thread.join()
+process_thread.join()
+display_thread.join()
+
+# Release camera capture object and destroy windows
+cap.release()
+cv2.destroyAllWindows()
--- Object_Detection_Files/object_ident.py
+++ Object_Detection_Files/object_ident.py
@@ -1,15 +1,19 @@
 import cv2
+import os
+# os.system("chdir \\\\Mac\\Home\\Desktop\\pupper-master-github\\pupper")
 # x dimension corresponds to height and y dimension corresponds to width, just like 2D array
-frame_height = 720
-frame_width = 1280
+
 
 classNames = []
-classFile = "./coco.names"
+
+# Change path based on OS and directory
+classFile = "C:\\Users\\hardik\\pupper-master-github\\pupper\\Object_Detection_Files\\coco.names"
+configPath = "C:\\Users\\hardik\\pupper-master-github\\pupper\\Object_Detection_Files\\ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "C:\\Users\\hardik\\pupper-master-github\\pupper\\Object_Detection_Files\\frozen_inference_graph.pb"
+
 with open(classFile, "rt") as f:
     classNames = f.read().rstrip("\n").split("\n")
 
-configPath = "./ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
-weightsPath = "./frozen_inference_graph.pb"
 
 net = cv2.dnn_DetectionModel(weightsPath, configPath)
 net.setInputSize(320, 320)
@@ -18,7 +22,8 @@ net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
 
-def getObjects(n_objects, img, thres, nms, draw=True, objects=['person']):
+def getObjects(n_objects, img, thres, nms,  frame_height, frame_width, draw=True, objects=['person']):
+    frame_center = (frame_width//2, frame_height//2)
     classIds, confs, bbox = net.detect(
         img, confThreshold=thres, nmsThreshold=nms)
     # print(classIds,bbox)
@@ -36,7 +41,6 @@ def getObjects(n_objects, img, thres, nms, draw=True, objects=['person']):
                     center_x = x1 + (height // 2)
                     center_y = y1 + (width // 2)
                     object_center = (center_x,center_y)
-                    frame_center = (frame_width//2, frame_height//2)
                     cv2.arrowedLine(img, object_center, frame_center, (0, 255, 0),
                                     thickness=2, tipLength=0.5)
                     cv2.arrowedLine(img, object_center, (frame_center[0], object_center[1]), (255, 0, 0),
@@ -45,8 +49,8 @@ def getObjects(n_objects, img, thres, nms, draw=True, objects=['person']):
 
                     # cv2.circle(img, frame_center, radius=5,
                     #            color=(0, 0, 255), thickness=-1)
-                    # cv2.circle(img, object_center, radius=5,
-                    #            color=(255, 255, 255), thickness=-1)
+                    cv2.circle(img, object_center, radius=5,
+                               color=(255, 255, 255), thickness=-1)
                     # cv2.circle(img, (x1+height//2,y1+width//2), radius=5,
                     #            color=(0, 255, 255), thickness=-1)
 
@@ -55,11 +59,21 @@ def getObjects(n_objects, img, thres, nms, draw=True, objects=['person']):
                     cv2.putText(img, str(round(confidence*100, 2)), (box[0]+200, box[1]+30),
                                 cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
                     # print(n_objects)
-
+                elif (n_objects>0):
+                    x1, y1, height, width = box[0], box[1], box[2], box[3]
+                    n_objects -= 1
+                    center_x = x1 + (height // 2)
+                    center_y = y1 + (width // 2)
+                    object_center = (center_x,center_y)
+                    area = height*width
+                    objectInfo.append(area)
+                    objectInfo.append(object_center)
     return img, objectInfo
 
 
 if __name__ == "__main__":
+    frame_height = 720
+    frame_width = 1280
     thresh = 0.7  # Threshold to detect object
     nms = 0.8
     cap = cv2.VideoCapture(0)
@@ -75,7 +89,7 @@ if __name__ == "__main__":
     while True:
         n_objects = 1
         success, img = cap.read()
-        result, objectInfo = getObjects(n_objects, img, thresh, nms)
+        result, objectInfo = getObjects(n_objects, img, thresh, nms, frame_height, frame_width)
         # print(objectInfo)
         # print(result)
         cv2.imshow("Output", img)
